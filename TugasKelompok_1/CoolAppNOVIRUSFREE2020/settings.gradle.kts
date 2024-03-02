f80ec89b5475033b0621ad122f7c67e349a86b6c
aryoshevar <152252850+aryoshevar@users.noreply.github.com>
2024-03-02 18:36:06 +0700

    Add files via upload

---
+++ TugasKelompok_1/CoolAppNOVIRUSFREE2020/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CoolApp NO VIRUS FREE 2020"
+include(":app")
